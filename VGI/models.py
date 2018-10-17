7236e285ebf7302ef5ae166b20a6dede5d6ea5d7
mortezaomidi <omidiporm@gmail.com>
2018-10-18 00:38:48 +0330

    uqol model initialization

--- VGI/models.py
+++ VGI/models.py
@@ -1,3 +1,80 @@
-from django.db import models
+from django.contrib.gis.db import models as geomodel
 
-# Create your models here.
+
+class Criteria(geomodel.Model):
+
+    Q1 = (
+        (1, "Negligible"),
+        (2, "Very low"),
+        (3, "Low"),
+        (4, "Moderate"),
+        (5, "High"),
+        (7, "Very High"),
+        (8, "Extreme"),
+        )
+    Q2 = (
+        (1, "Very Bad"),
+        (2, "Bad"),
+        (3, "Somewhat Bad"),
+        (4, "Neither Good nor Bad"),
+        (5, "Somewhat Good"),
+        (6, "Good"),
+        (7, "Very Good"),
+        )
+    Q3 = (
+        (1, "Strongly Disagree"),
+        (2, "Disagree"),
+        (3, "Somewhat Disagree"),
+        (4, "Neither Agree Nor Disagree"),
+        (5, "Somewhat Agree"),
+        (6, "Agree"),
+        (7, "Strongly Agree"),
+        )
+
+    # Enviromental Critera
+    q11 = geomodel.PositiveSmallIntegerField(choices=Q1, verbose_name=None)
+    q12 = geomodel.PositiveSmallIntegerField(choices=Q1, verbose_name=None)
+    q13 = geomodel.PositiveSmallIntegerField(choices=Q1, verbose_name=None)
+    q14 = geomodel.PositiveSmallIntegerField(choices=Q2, verbose_name=None)
+    q15 = geomodel.PositiveSmallIntegerField(choices=Q2, verbose_name=None)
+
+    @property
+    def sum_q1(self):
+        return self.q11 + self.q12 + self.q13 + self.q14 + self.q15
+
+    # Socio-economic Criteria
+    q21 = geomodel.PositiveSmallIntegerField(choices=Q1, verbose_name=None)
+    q22 = geomodel.PositiveSmallIntegerField(choices=Q1, verbose_name=None)
+    q23 = geomodel.PositiveSmallIntegerField(choices=Q1, verbose_name=None)
+    q24 = geomodel.PositiveSmallIntegerField(choices=Q1, verbose_name=None)
+    q25 = geomodel.PositiveSmallIntegerField(choices=Q1, verbose_name=None)
+    q26 = geomodel.PositiveSmallIntegerField(choices=Q1, verbose_name=None)
+    q27 = geomodel.PositiveSmallIntegerField(choices=Q1, verbose_name=None)
+    q28 = geomodel.PositiveSmallIntegerField(choices=Q1, verbose_name=None)
+    q29 = geomodel.PositiveSmallIntegerField(choices=Q3, verbose_name=None)
+    q210 = geomodel.PositiveSmallIntegerField(choices=Q3, verbose_name=None)
+
+    @property
+    def sum_q2(self):
+        return self.q21 + self.q22 + self.q23 + self.q24 + self.q25
+         + self.q26 + self.q27 + self.q28 + self.q29 + self.q210
+
+    # Phisical Criteria
+    q31 = geomodel.PositiveSmallIntegerField(choices=Q1, verbose_name=None)
+    q32 = geomodel.PositiveSmallIntegerField(choices=Q1, verbose_name=None)
+    q32 = geomodel.PositiveSmallIntegerField(choices=Q1, verbose_name=None)
+    q33 = geomodel.PositiveSmallIntegerField(choices=Q2, verbose_name=None)
+    q34 = geomodel.PositiveSmallIntegerField(choices=Q2, verbose_name=None)
+    q35 = geomodel.PositiveSmallIntegerField(choices=Q2, verbose_name=None)
+    q36 = geomodel.PositiveSmallIntegerField(choices=Q2, verbose_name=None)
+    q37 = geomodel.PositiveSmallIntegerField(choices=Q2, verbose_name=None)
+    q38 = geomodel.PositiveSmallIntegerField(choices=Q2, verbose_name=None)
+    q39 = geomodel.PositiveSmallIntegerField(choices=Q2, verbose_name=None)
+    q310 = geomodel.PositiveSmallIntegerField(choices=Q2, verbose_name=None)
+    q311 = geomodel.PositiveSmallIntegerField(choices=Q3, verbose_name=None)
+    q312 = geomodel.PositiveSmallIntegerField(choices=Q3, verbose_name=None)
+    q313 = geomodel.PositiveSmallIntegerField(choices=Q3, verbose_name=None)
+    q314 = geomodel.PositiveSmallIntegerField(choices=Q3, verbose_name=None)
+    q315 = geomodel.PositiveSmallIntegerField(choices=Q3, verbose_name=None)
+    q316 = geomodel.PositiveSmallIntegerField(choices=Q3, verbose_name=None)
+    q317 = geomodel.PositiveSmallIntegerField(choices=Q3, verbose_name=None)
