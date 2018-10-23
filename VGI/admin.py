ca80cdd24ef5417dedfe1d59db6267621e0bba50
mortezaomidi <omidiporm@gmail.com>
2018-10-24 02:08:33 +0330

    add Bing GeoAdmin customize from root django admin

--- VGI/admin.py
+++ VGI/admin.py
@@ -1,8 +1,7 @@
-from django.contrib import admin
+from django.contrib.gis import admin
 from .models import Criteria, MyUser, Region, Unit
 
-
-admin.site.register(Criteria)
 admin.site.register(MyUser)
-admin.site.register(Region)
-admin.site.register(Unit)
+admin.site.register(Criteria, admin.BingGeoAdmin)
+admin.site.register(Region, admin.BingGeoAdmin)
+admin.site.register(Unit, admin.BingGeoAdmin)
